46486b9f2557e50026734206627a534bdfdbeb04
so11y <247076126@qq.com>
2022-01-14 14:25:18 +0800

    ^o^

--- src/defineLentConfig.ts
+++ src/defineLentConfig.ts
@@ -1,7 +1,7 @@
 import { LentHttpInstance } from './types';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-export const defineLentConfig = <T extends Partial<LentHttpInstance>>(
+export const defineLentConfig = <T extends Partial<LentHttpInstance['config']>>(
 	l: T
 ): T => {
 	return l;
