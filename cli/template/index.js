94567beae0d292bef6b1025ccad38e114c103f67
钢琴家 <247076126@qq.com>
2022-01-16 16:51:14 +0800

    ^o^

--- cli/template/index.js
+++ cli/template/index.js
@@ -3,26 +3,32 @@ const ejs = require('ejs');
 const path = require('path');
 
 const createTemplate = (options) => {
-	const root = path.join(process.cwd(), '/xxx');
-	fs.mkdirSync(root);
+	const root = path.join(process.cwd(), '/src');
 	const useDefailt = Object.values(options).every((v) => v === false);
 	const isJs = useDefailt || options.js;
 	const renderSrcCode = [
-		renderFile('createHtml.ejs', 'createHtml.html', {
+		renderFile('createHtml.ejs', 'index.html', {
 			g: isJs
 		}),
-		renderFile('enterFile.ejs', `index.${isJs ? '.js' : '.ts'}`)
+		renderFile('enterFile.ejs', `index${isJs ? '.js' : '.ts'}`)
 	];
 	const renderRoot = [renderFile('lent.config.ejs', 'lent.config.js')];
 	if (!isJs) {
 		renderSrcCode.push(renderFile('index.d.ejs', 'index.d.ts'));
-		renderRoot.push(renderFile('tsconfig.ejs', 'tsconfig.ts'));
+		renderRoot.push(renderFile('tsconfig.ejs', 'tsconfig.json'));
 	}
-	renderRoot.forEach((v) => fs.writeFileSync(process.cwd(), v.source));
-	renderSrcCode.forEach((v) =>
-		fs.writeFileSync(path.join(root, v.fileName), v.source)
-	);
-	console.log('[lent cli] created template end');
+	if (fs.existsSync(root)) {
+		console.log('[lent cli] have src path remove to again');
+	} else {
+		fs.mkdirSync(root);
+		renderRoot.forEach((v) =>
+			fs.writeFileSync(path.join(process.cwd(), v.fileName), v.source)
+		);
+		renderSrcCode.forEach((v) =>
+			fs.writeFileSync(path.join(root, v.fileName), v.source)
+		);
+		console.log('[lent cli] created template end');
+	}
 };
 
 const renderFile = (filePath, fileName, replace = {}) => {
