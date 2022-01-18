bb53a86f0e826fef6dc6c47c84ec3d63a0f19cd0
钢琴家 <247076126@qq.com>
2022-01-19 01:06:22 +0800

    ^o^

--- playground/index.ts
+++ playground/index.ts
@@ -1,24 +1,24 @@
 import { t } from './code/a';
 import './index.css';
-import { isArray } from 'rolling-way';
-import vvv from 'vue';
+// import { isArray } from 'rolling-way';
+// import vvv from 'vue';
 
-vvv.createApp({
-    template: "<div>{{helloLent}}</div>",
-    setup() {
-        return {
-            helloLent: 'hello lent'
-        };
-    }
-}).mount('#app');
+// vvv.createApp({
+//     template: "<div>{{helloLent}}</div>",
+//     setup() {
+//         return {
+//             helloLent: 'hello lent'
+//         };
+//     }
+// }).mount('#app');
 
-console.log(isArray([]));
+// console.log(isArray([]));
 
 console.log('hello wolrd');
 console.log(t, 'good');
 
 if (import.meta.hot) {
-    import.meta.hot.accept(() => {
-        console.log('----');
-    });
+	import.meta.hot.accept(() => {
+		console.log('----');
+	});
 }
